263fb4e0226114022eee325e8da9a93db6eee0ca
Brent Van Geertruy <brentvangeertruy@outlook.com>
2018-03-15 10:31:05 +0100

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+// jest.config.js
+module.exports = {
+  verbose: false,
+  notify: false,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+    "json"
+  ],
+  transform: {
+    "\\.(ts|tsx)$": "<rootDir>/node_modules/ts-jest/preprocessor.js"
+  },
+  testRegex: "/tests/.*\\.(ts|tsx|js)$",
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    'src/index.ts',
+  ],
+};
