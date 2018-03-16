a3a0dd780a2f6f46edaa74080cfa477c5770258f
Brent Van Geertruy <brentvangeertruy@outlook.com>
2018-03-16 14:33:42 +0100

    SIAP-47 Swagger ui integration via treehouse

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,12 @@ treehouse.setBasicSecurity(app, '*');
 treehouse.setBodyParser(app, '*');
 treehouse.setRateLimiter(app, '*');
 
+// TODO: Versioning (loop over folder from docs and set route per version)
+treehouse.setSwagger(app, '/documentation/v1', 'docs/v1.0.0.yml', {
+  host: process.env.SWAGGER_BASE_URL,
+  schemes: ['http', 'https'],
+});
+
 // Error handling
 app.use((error, _req, res, _next) => responder.error(res, error));
 app.all('*', (_req, res) => res.sendStatus(404));
