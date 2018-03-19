b496f9f7a8b16d4519eaf64ca0ed4dbd61df12ae
Brent Van Geertruy <brentvangeertruy@outlook.com>
2018-03-19 16:44:07 +0100

    SIAP-47: Add tree-house-errors with errorParser

--- src/lib/responder.ts
+++ src/lib/responder.ts
@@ -1,8 +1,8 @@
 import * as httpStatus from 'http-status';
 import { Response } from 'express-serve-static-core';
+import { parseErrors } from 'tree-house-errors';
 import { ErrorSerializer } from 'jsonade';
 import { logger } from '../lib/logger';
-
 
 /**
  * ExpressJS responder to send success/error responses
@@ -17,20 +17,14 @@ export const responder = {
     logger.debug('Response: ', serializer.serialize(payload));
     return res.status(status).json(serializer.serialize(payload));
   },
-  error: (res: Response, error) => {
-    // TODO: Use tree-house-errors for parsing
-    const parsedError = {
-      status: 500,
-      message: error.message,
-    };
-
+  error: (res: Response, errors: any) => {
+    const parsedError = parseErrors(errors);
     const serializerError = ErrorSerializer.serialize([parsedError]);
 
     logger.error('Error response: ', serializerError);
     return res.status(parsedError.status).json(serializerError);
   },
 };
-
 
 // Type definitions
 export interface ResponderOptions {
