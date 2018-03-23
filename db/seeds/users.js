7456b20151a230ce9e4c10056c0db1c453313b20
Brent Van Geertruy <brentvangeertruy@outlook.com>
2018-03-23 13:22:42 +0100

    Seed random 50 users via faker.js

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -1,17 +1,34 @@
 
+const faker = require('faker');
+
 exports.seed = function (knex, Promise) {
   return knex('users').del()
     .then(() => {
+      const users = [];
+      const roles = ['ADMIN', 'USER'];
+
+      for (let i = 0; i < 50; i += 1) {
+        const randomRole = Math.floor(Math.random() * roles.length);
+        users.push({
+          email: faker.internet.email(),
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          password: faker.internet.password(),
+          hasAccess: faker.random.boolean(),
+          role: roles[randomRole],
+        });
+      }
+
+      users.push({
+        email: 'development@icapps.com',
+        firstName: 'development',
+        lastName: 'iCapps',
+        password: '',
+        hasAccess: true,
+        role: 'ADMIN',
+      });
+
       // Inserts seed entries
-      return knex('users').insert([
-        {
-          email: 'development@icapps.com',
-          firstName: 'development',
-          lastName: 'iCapps',
-          password: '',
-          hasAccess: true,
-          role: 'ADMIN',
-        }
-      ]);
+      return knex('users').insert(users);
     });
 };
