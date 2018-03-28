ad0c9b32405aa7d138d0b396e68e65bf993e9be0
Brent Van Geertruy <brentvangeertruy@outlook.com>
2018-03-28 15:11:17 +0200

    SIAP-59: Use mandrill api in mailer lib

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -1,26 +1,80 @@
-import { createTransport, Transporter, SendMailOptions } from 'nodemailer';
+import * as mandrill from 'mandrill-api';
 import { logger } from './logger';
-
-const defaultTransport: Transporter = createTransport({
-  service: 'Mandrill',
-  auth: {
-    user: process.env.MANDRILL_USERNAME,
-    pass: process.env.MANDRILL_API_KEY,
-  },
-  logger: process.env.LOG_LEVEL === 'debug', // log to console
-  debug: process.env.LOG_LEVEL === 'debug', // include SMTP traffic in the logs
-});
 
 
 /**
- * Send an email with all available nodemailer options
+ * Return a default MailClient (Mandrill in this case)
  */
-export async function send(options: SendMailOptions, transport: Transporter = defaultTransport): Promise<void> {
-  try {
-    const info = await transport.sendMail(options);
-    logger.info(`Message sent: ${info ? info.response : ''}`);
-  } catch (error) {
-    logger.error(`Error trying to send an email: ${error.message}`);
-    throw error;
-  }
+export function getDefaultClient() {
+  return new mandrill.Mandrill(process.env.MANDRILL_API_KEY, process.env.LOG_LEVEL === 'debug');
 }
+
+
+/**
+ * Send an email with html or text
+ */
+export async function send(options: MailOptions, client: MailClient) {
+  return new Promise((resolve, reject) => {
+    client.messages.send(options, (result) => {
+      logger.info(`Message sent: ${result}`);
+      resolve();
+    }, (error) => {
+      logger.error(`Error trying to send an email: ${error.message}`);
+      reject(error);
+    });
+  });
+}
+
+
+/**
+ * Send an email with a template from Mandrill/Mailchimp provided
+ */
+export async function sendTemplate(options: TemplateMailOptions, client: MailClient) {
+  return new Promise((resolve, reject) => {
+    client.messages.sendTemplate(options, (result) => {
+      logger.info(`Message sent: ${result}`);
+      resolve();
+    }, (error) => {
+      logger.error(`Error trying to send an email: ${error.message}`);
+      reject(error);
+    });
+  });
+}
+
+
+// Interfaces
+export interface MailClient {
+  messages: {
+    send: Function;
+    sendTemplate: Function;
+  };
+}
+
+export interface MailOptions {
+  message: {
+    html?: string;
+    text?: string;
+    subject: string;
+    from_email: string;
+    from_name?: string;
+    to: {
+      email: string;
+      name?: string;
+      type?: 'to'
+    }[];
+    headers?: {};
+    bcc_address?: string;
+  };
+  async?: boolean;
+  send_at?: string;
+  ip_pool?: string;
+}
+
+export interface TemplateMailOptions extends MailOptions {
+  template_name: string;
+  template_content?: {
+    name: string;
+    content: string;
+  }[];
+}
+
