7a6bd00aa4f70ea49937fd94e016b69f8e7c6804
horstenwillem <willem.horsten@icapps.com>
2018-03-29 13:22:49 +0200

    Update validator on meta routes

--- src/routes/v1/meta.routes.ts
+++ src/routes/v1/meta.routes.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
-import { handleAsyncFn } from 'tree-house';
+import { handleAsyncFn, validateSchema } from 'tree-house';
 import { hasPermission } from '../../middleware/permission.middleware';
 import { roles } from '../../config/roles.config';
-import { validateSchema } from '../../lib/validator';
 import { metaSchema } from '../../schemes/meta.schema';
 import * as controller from '../../controllers/meta.controller';
 
+const defaultOptions = { allowUnknownQuery: false };
 export const routes: Router = Router({ mergeParams: true })
   .get('/code-types', (req, res, next) =>
-    hasPermission(req, res, next, roles.ADMIN), validateSchema(metaSchema.findAllCodeTypes), handleAsyncFn(controller.findAllCodeTypes))
+    hasPermission(req, res, next, roles.ADMIN),
+    validateSchema(metaSchema.findAllCodeTypes, defaultOptions),
+    handleAsyncFn(controller.findAllCodeTypes))
   .get('/codes', (req, res, next) =>
-    hasPermission(req, res, next, roles.ADMIN), validateSchema(metaSchema.findByCodeId), handleAsyncFn(controller.findAllCodes));
+    hasPermission(req, res, next, roles.ADMIN),
+    validateSchema(metaSchema.findByCodeId, defaultOptions),
+    handleAsyncFn(controller.findAllCodes));
