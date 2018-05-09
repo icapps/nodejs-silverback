dc357e7dce36f673437a3d3dee2a4bf4cef6a9bd
Brent Van Geertruy <brentvangeertruy@outlook.com>
2018-05-09 11:14:46 +0200

    Import directly from translations and use new tree-house-errors version

---
+++ src/lib/translator.ts
@@ -0,0 +1,32 @@
+import * as icappsTranslation from 'tree-house-translations';
+import { existsSync, mkdirSync } from 'fs';
+
+const defaultOptions = <any> {
+  destination: process.cwd() + '/locales',
+  clean: true,
+  seperateCategories: false,
+  exportType: 'json',
+};
+
+
+/**
+ * Import translations from the icapps translations service
+ */
+export async function importTranslations(url: string, token: string, options?: TranslationOptions) {
+  const allOptions = Object.assign({}, defaultOptions, options);
+
+  // if locales location does not exists, make directory
+  if (!existsSync(allOptions.destination)) {
+    mkdirSync(allOptions.destination);
+  }
+
+  return icappsTranslation.import(url, token, allOptions);
+}
+
+export interface TranslationOptions {
+  destination?: string;
+  clean?: boolean;
+  verbose?: boolean;
+  seperateCategories?: boolean;
+  exportType?: string;
+}
