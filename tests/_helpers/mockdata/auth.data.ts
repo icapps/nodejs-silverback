10d801d7a39a79d90728ec83c23c73a2290ee98b
Brent Van Geertruy <brentvangeertruy@outlook.com>
2018-07-19 11:57:24 +0200

    SIAP-205: jwt middleware permission

--- tests/_helpers/mockdata/auth.data.ts
+++ tests/_helpers/mockdata/auth.data.ts
@@ -1,20 +1,45 @@
+import * as request from 'supertest';
 import { createJwt } from 'tree-house-authentication';
 import { jwtConfig } from '../../../src/config/auth.config';
-import { adminUser, blockedstateUser, createUser, unconfirmedUser } from './user.data';
+import { adminUser, blockedUser, createUser, unconfirmedUser } from './user.data';
 import { User } from '../../../src/models/user.model';
+import { app } from '../../../src/app';
 
+const prefix = `/api/${process.env.API_VERSION}`;
 const tokens = {};
+
+/**
+ * Login with username and password
+ * Return the cookie that needs to be set
+ */
+async function login(username: string, password: string): Promise<string> {
+  const { header } = await request(app)
+    .post(`${prefix}/auth/login`)
+    .send({ username, password });
+  return header['set-cookie'];
+}
 
 export function getValidJwt(userId: string) {
   return createJwt({ userId }, jwtConfig);
 }
 
-export async function getAdminToken() {
-  const user = await createUser(adminUser, 'registered');
-  return await getValidJwt(user.id);
+/**
+ * Get a valid user token (cookie)
+ * We don't want to login every time again, so store already logged in users
+ */
+export async function getUserSessionToken(user: User): Promise<string[]> {
+  if (!(user.email in tokens)) {
+    const token = await login(user.email, user.password);
+    tokens[user.email] = token;
+  }
+  return tokens[user.email];
 }
 
-export async function getUserToken(user: User): Promise<string[]> {
+/**
+ * Get a valid user token (cookie)
+ * We don't want to login every time again, so store already logged in users
+ */
+export async function getUserJwtToken(user: User): Promise<string[]> {
   if (!(user.email in tokens)) {
     const token = await getValidJwt(user.id);
     tokens[user.email] = token;
@@ -22,7 +47,7 @@ export async function getUserToken(user: User): Promise<string[]> {
   return tokens[user.email];
 }
 
-export async function getUserTokens(users: User[]): Promise<string[]> {
+export async function getUserJwtTokens(users: User[]): Promise<string[]> {
   const tokens = [];
   for (const userValues of users) {
     const token = await getValidJwt(userValues.id);
@@ -31,12 +56,17 @@ export async function getUserTokens(users: User[]): Promise<string[]> {
   return tokens;
 }
 
+export async function getAdminToken() {
+  const user = await createUser(adminUser, 'registered');
+  return await getValidJwt(user.id);
+}
+
 export async function getUnconfirmedUserToken() {
   const user = await createUser(unconfirmedUser, 'complete_registration');
   return await getValidJwt(user.id);
 }
 
 export async function getBlockedStateUserToken() {
-  const user = await createUser(blockedstateUser, 'blocked');
+  const user = await createUser(blockedUser, 'blocked');
   return getValidJwt(user.id);
 }
--- tests/middleware/permission.middleware.test.ts
+++ tests/middleware/permission.middleware.test.ts
@@ -3,108 +3,107 @@ import * as httpMocks from 'node-mocks-http';
 import { errors } from '../../src/config/errors.config';
 import { roles } from '../../src/config/roles.config';
 import { hasPermission } from '../../src/middleware/permission.middleware';
-import { getBlockedStateUserToken, getUnconfirmedUserToken, getUserTokens, getValidJwt } from '../_helpers/mockdata/auth.data';
+import { getBlockedStateUserToken, getUnconfirmedUserToken, getUserJwtTokens, getValidJwt } from '../_helpers/mockdata/auth.data';
 import { clearAll } from '../_helpers/mockdata/data';
-import { adminUser, createUsers, regularUser } from '../_helpers/mockdata/user.data';
+import { adminUser, createUsers, regularUser, blockedUser, unconfirmedUser, createUser } from '../_helpers/mockdata/user.data';
 
 describe('hasPermission middleware', () => {
-  const prefix = `/api/${process.env.API_VERSION}`;
-  const users = { regular: null, admin: null };
-  const tokens = { regular: null, admin: null };
-  let unconfirmedToken;
-  let blockedToken;
+  const tokens = { regular: null, admin: null, unconfirmed: null, blocked: null };
 
   beforeAll(async () => {
     await clearAll(); // Full db clear
 
-    // Create a regular and admin user
+    // Create all different type of users
     const { data: createdUsers } = await createUsers([regularUser, adminUser], 'registered');
-    const sorted = createdUsers.sort((a, b) => a.role.code.localeCompare(b.role.code));
-    [users.admin, users.regular] = sorted;
+    const [admin, regular] = createdUsers.sort((a, b) => a.role.code.localeCompare(b.role.code));
+
+    const blocked = await createUser(blockedUser, 'blocked');
+    const unconfirmed = await createUser(unconfirmedUser, 'complete_registration');
 
     // All user type tokens
-    const createdTokens = await getUserTokens([users.regular, users.admin]);
-    [tokens.regular, tokens.admin] = createdTokens;
-
-    unconfirmedToken = await getUnconfirmedUserToken(); // Also creates user
-    blockedToken = await getBlockedStateUserToken(); // Also creates user
+    const createdTokens = await getUserJwtTokens([regular, admin, unconfirmed, blocked]);
+    [tokens.regular, tokens.admin, tokens.unconfirmed, tokens.blocked] = createdTokens;
   });
 
   afterAll(async () => {
     await clearAll(); // Full db clear - empty db after tests
   });
 
-  it('Should succesfully set current user on session request when authenticated', async () => {
-    const request = httpMocks.createRequest({ headers: { Authorization: `Bearer ${tokens.regular}` } });
-    const response = httpMocks.createResponse();
+  describe('Jwt authentication', () => {
+    it('Should succesfully set current user on session request when authenticated', async () => {
+      const request = httpMocks.createRequest({ headers: { Authorization: `Bearer ${tokens.regular}` } });
+      const response = httpMocks.createResponse();
 
-    expect.assertions(1);
-    await hasPermission(request, response, (nxt) => {
-      expect(request.current.user.email).toEqual(regularUser.email);
+      expect.assertions(1);
+      await hasPermission(request, response, (nxt) => {
+        expect(request.current.user.email).toEqual(regularUser.email);
+      });
     });
-  });
 
-  it('Should succesfully set current user on session request when authenticated with role', async () => {
-    const request = httpMocks.createRequest({ headers: { Authorization: `Bearer ${tokens.admin}` } });
-    const response = httpMocks.createResponse();
+    it('Should succesfully set current user on session request when authenticated with role', async () => {
+      const request = httpMocks.createRequest({ headers: { Authorization: `Bearer ${tokens.admin}` } });
+      const response = httpMocks.createResponse();
 
-    expect.assertions(1);
-    await hasPermission(request, response, (nxt) => {
-      expect(request.current.user.email).toEqual(adminUser.email);
-    }, roles.ADMIN);
-  });
-
-  it('Should throw an error when user does not have the correct role', async () => {
-    const request = httpMocks.createRequest({ headers: { Authorization: `Bearer ${tokens.regular}` } });
-    const response = httpMocks.createResponse();
-
-    expect.assertions(2);
-    const result = await hasPermission(request, response, (nxt) => {
-      expect(nxt.code).toEqual(errors.NO_PERMISSION.code);
-      expect(nxt.message).toEqual(errors.NO_PERMISSION.message);
-    }, roles.ADMIN);
-  });
-
-  it('Should throw an error when user is not found', async () => {
-    const token = await getValidJwt(faker.random.uuid());
-    const request = httpMocks.createRequest({ headers: { Authorization: `Bearer ${token}` } });
-    const response = httpMocks.createResponse();
-
-    expect.assertions(2);
-    const result = await hasPermission(request, response, (nxt) => {
-      expect(nxt.code).toEqual(errors.USER_NOT_FOUND.code);
-      expect(nxt.message).toEqual(errors.USER_NOT_FOUND.message);
+      expect.assertions(1);
+      await hasPermission(request, response, (nxt) => {
+        expect(request.current.user.email).toEqual(adminUser.email);
+      }, roles.ADMIN);
     });
-  });
 
-  it('Should throw an error when no jwt token is provided', async () => {
-    const request = httpMocks.createRequest();
-    const response = httpMocks.createResponse();
+    it('Should throw an error when user does not have the correct role', async () => {
+      const request = httpMocks.createRequest({ headers: { Authorization: `Bearer ${tokens.regular}` } });
+      const response = httpMocks.createResponse();
 
-    expect.assertions(2);
-    const result = await hasPermission(request, response, (nxt) => {
-      expect(nxt.code).toEqual(errors.INVALID_TOKEN.code);
-      expect(nxt.message).toEqual(errors.INVALID_TOKEN.message);
+      expect.assertions(2);
+      await hasPermission(request, response, (nxt) => {
+        expect(nxt.code).toEqual(errors.NO_PERMISSION.code);
+        expect(nxt.message).toEqual(errors.NO_PERMISSION.message);
+      }, roles.ADMIN);
     });
-  });
 
-  it('Should throw an error when user has not yet confirmed his email', async () => {
-    const request = httpMocks.createRequest({ headers: { Authorization: `Bearer ${unconfirmedToken}` } });
-    const response = httpMocks.createResponse();
+    it('Should throw an error when user is not found', async () => {
+      const token = await getValidJwt(faker.random.uuid());
+      const request = httpMocks.createRequest({ headers: { Authorization: `Bearer ${token}` } });
+      const response = httpMocks.createResponse();
 
-    const result = await hasPermission(request, response, (nxt) => {
-      expect(nxt.code).toEqual(errors.USER_UNCONFIRMED.code);
-      expect(nxt.message).toEqual(errors.USER_UNCONFIRMED.message);
+      expect.assertions(2);
+      await hasPermission(request, response, (nxt) => {
+        expect(nxt.code).toEqual(errors.USER_NOT_FOUND.code);
+        expect(nxt.message).toEqual(errors.USER_NOT_FOUND.message);
+      });
     });
-  });
 
-  it('Should throw an error when user is blocked', async () => {
-    const request = httpMocks.createRequest({ headers: { Authorization: `Bearer ${blockedToken}` } });
-    const response = httpMocks.createResponse();
+    it('Should throw an error when no jwt token or session token is provided', async () => {
+      const request = httpMocks.createRequest();
+      const response = httpMocks.createResponse();
 
-    const result = await hasPermission(request, response, (nxt) => {
-      expect(nxt.code).toEqual(errors.USER_BLOCKED.code);
-      expect(nxt.message).toEqual(errors.USER_BLOCKED.message);
+      expect.assertions(2);
+      await hasPermission(request, response, (nxt) => {
+        expect(nxt.code).toEqual(errors.MISSING_HEADERS.code);
+        expect(nxt.message).toEqual(errors.MISSING_HEADERS.message);
+      });
     });
+
+    it('Should throw an error when user has not yet confirmed his email', async () => {
+      const request = httpMocks.createRequest({ headers: { Authorization: `Bearer ${tokens.unconfirmed}` } });
+      const response = httpMocks.createResponse();
+
+      await hasPermission(request, response, (nxt) => {
+        expect(nxt.code).toEqual(errors.USER_UNCONFIRMED.code);
+        expect(nxt.message).toEqual(errors.USER_UNCONFIRMED.message);
+      });
+    });
+
+    it('Should throw an error when user is blocked', async () => {
+      const request = httpMocks.createRequest({ headers: { Authorization: `Bearer ${tokens.blocked}` } });
+      const response = httpMocks.createResponse();
+
+      await hasPermission(request, response, (nxt) => {
+        expect(nxt.code).toEqual(errors.USER_BLOCKED.code);
+        expect(nxt.message).toEqual(errors.USER_BLOCKED.message);
+      });
+    });
+
   });
+
 });
