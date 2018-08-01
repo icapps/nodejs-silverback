6cb9ebf9af6ec50ae35cf4dcfacdc6e569d0d0a5
horstenwillem <willem.horsten@icapps.com>
2018-08-01 11:57:01 +0200

    Upgrade winston

--- src/config/logger.config.ts
+++ src/config/logger.config.ts
@@ -2,9 +2,11 @@ import * as winston from 'winston';
 
 export default {
   level: process.env.LOG_LEVEL || 'info',
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.simple(),
+  ),
   transports: [
-    new winston.transports.Console({
-      colorize: true,
-    }),
+    new winston.transports.Console(),
   ],
 };
